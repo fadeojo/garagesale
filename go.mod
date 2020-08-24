8f1d5e25f781ab6c39d4e9b0a036d955068368e6
Femi Adeojo <femi.adeojo@uberflip.com>
2020-08-24 17:33:09 -0400

    02h-shutdown

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fadeojo/garagesale
 
 go 1.13
 
-require golang.org/x/tools v0.0.0-20200823205832-c024452afbcd // indirect
+require (
+	golang.org/x/tools v0.0.0-20200823205832-c024452afbcd // indirect
+	honnef.co/go/tools v0.0.1-2020.1.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,88 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	//Convert the Echo function to a type that implements http.Handler
-	handler := http.HandlerFunc(Echo)
+	// =========================================================================
+	// App Starting
 
-	if err := http.ListenAndServe("localhost:8004", handler); err != nil {
-		log.Fatalf("error: listening and serving: %s \n", err)
+	log.Printf("main : Started")
+	defer log.Println("main : Completed")
+
+	// =========================================================================
+	// Start API Service
+	api := http.Server{
+		Addr:         "localhost:8000",
+		Handler:      http.HandlerFunc(Echo),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for requests.
+	go func() {
+		log.Printf("main: API listening on: %s \n", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use a buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	// =========================================================================
+	// Shutdown
+
+	// Blocking main and waiting for shutdown.
+	select {
+	case err := <- serverErrors:
+		log.Fatalf("error listening and serving: %s", err)
+	case <- shutdown:
+		log.Println("main: Shutdown")
+
+		// Give outstanding requests a deadline for completion.
+		timeout := 5 * time.Second
+		ctx,cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		// Asking listener to shutdown and load shed.
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Printf("main: Graceful shutdown down did not happen in %v: %v", timeout, err)
+			err = api.Close()
+		}
+
+		if err != nil {
+			log.Fatalf("main: could not stop server gracefully %v", err)
+		}
+
 	}
 
 }
 
 //Echo is a basic HTP handler
 func Echo(w http.ResponseWriter, r *http.Request) {
+	// Print a random number at the beginning and end of each request.
+	n := rand.Intn(1000)
+	log.Println("start", n)
+	defer log.Println("stop", n)
+
+	// Simulate a long-running request.
+	time.Sleep(3 * time.Second)
+
 	if _, err := fmt.Fprintf(w, "You have asked to %s %s \n", r.Method, r.URL.Path); err != nil {
-		 log.Fatalf("Error while writing response: %s \n", err)
+		log.Fatalf("Error while writing response: %s \n", err)
 	}
 }
